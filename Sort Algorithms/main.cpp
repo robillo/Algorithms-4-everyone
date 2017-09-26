656f30b8229ec236c3f288bd9801e7e8f4aff695
Daniela Brozzoni <brozzoni@live.it>
2017-09-26 10:52:09 +0200

    Update main.cpp

--- Sort Algorithms/main.cpp
+++ Sort Algorithms/main.cpp
@@ -1,7 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <iostream>
+#include "Define.cpp"
+
 #include "BubbleSort.cpp"
 #include "AdaptiveInsertionSort.cpp"
 #include "InsertionSort.cpp"
@@ -9,11 +7,14 @@
 #include "QuickSort.cpp"
 #include "RandomizedQuickSort.cpp"
 #include "RandomizedQuickSort with strings.cpp"
+#include "RandomizedQuickSort with strings and pointers.cpp"
+#include "Print.cpp"
 
-#include "Define.cpp"
 
 int arr[MAXN];
 char arrS[MAXN][MAXS+1];
+int indici[MAXN];
+char *puntatoriIndici[MAXN];
 
 int main()
 {
@@ -27,8 +28,11 @@ int main()
     for(int i = 0; i < MAXN; i++)
         arrS[i][MAXS] = '\0';
 
-    stampaS(arrS, MAXN);
-    printf("\n");
-    QuickSortS(arrS, MAXN);
-    stampaSI(s, indici, N);
+    for(int i = 0; i < MAXN; i++)
+        puntatoriIndici[i] = arrS[i];
+
+    print_Strings_Pointers(puntatoriIndici, MAXN);
+    QuickSortSP(puntatoriIndici, MAXN);
+    print_Strings_Pointers(puntatoriIndici, MAXN);
+
 }
